44c82bf45278114219e29334a21d58237c3afaa2
Gabriela Cabral Prado <gabrielacabralprado@Gabrielas-MacBook-Air.local>
2019-12-20 02:40:38 -0300

    Adicionando visao desktop

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../../assets/database/home-products.json';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  productsHome;
+
   constructor() { }
 
+  
+
   ngOnInit() {
+    this.productsHome = products;
   }
 
 }
